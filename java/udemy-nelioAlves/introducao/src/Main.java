0662ef1333fe0bb5b3b34cfba77ec03f42320d68
Jose Meurer <josewmeurer@gmail.com>
2024-05-13 16:32:37 -0300

    first project java

---
+++ java/udemy-nelioAlves/introducao/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+
+        System.out.println("Contador de 1 até 10");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("Numero: %d%n", i);
+        }
+    }
+}
