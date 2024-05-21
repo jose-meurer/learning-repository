b243d49971539cecafc29fd4fc00924425f78a46
Jose Meurer <josewmeurer@gmail.com>
2024-05-21 17:33:33 -0300

    js aula07

---
+++ javascript/aula07/index.js
@@ -0,0 +1,9 @@
+var nome = "José";
+var nome = "Daniel";
+var nome = "Nicole"; //Um dos problemas do var
+
+let nomeLet = "Joao";
+let nomeLet = "Maria"; // SyntaxError: Identifier 'nomeLet' has already been declared
+
+console.log(nome);
+console.log(nomeLet);
