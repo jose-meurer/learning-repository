6b3b6bfee1c7e5da57e0036cd4e7301f2c5c8a07
Jose Meurer <josewmeurer@gmail.com>
2024-05-21 20:05:34 -0300

    js Aula09

---
+++ javascript/aula09/index.js
@@ -0,0 +1,30 @@
+//String
+const nome = "José";
+const nome2 = 'José';
+const nome3 = `José`;
+console.log(nome, typeof nome);
+
+//Number
+const num1 = 10;
+const num2 = 15.32;
+console.log(num1, typeof num1);
+
+//Undefined and Null
+let nomeAluno; //undefined = Não aponta para local nenhum da memória
+let sobrenomeAluno = null; //Nulo = Tambem nao aponta para local nenhum da memória;
+console.log(nomeAluno, typeof nomeAluno);
+console.log(sobrenomeAluno, typeof sobrenomeAluno);
+
+//Booleano
+const aprovado = false;
+console.log(boolean, typeof boolean);
+ 
+/**
+ * Tipos primitivos:
+ * string
+ * number
+ * undefined
+ * null
+ * boolean
+ * symbol
+ */
