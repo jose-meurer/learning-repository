6ef50eecbe2c8d7aed490dab5bee47cdf37ed52b
Jose Meurer <josewmeurer@gmail.com>
2024-05-25 20:01:53 -0300

    js aula17 functions

---
+++ javascript/aula17/index.js
@@ -0,0 +1,21 @@
+function raizFunction(n) {
+  return n ** 0.5;
+}
+
+console.log("Funcao decladarada: " + raizFunction(9));
+
+const raizAnonima = function (n) {
+  return n ** 0.5;
+};
+
+console.log("Funcao anonima: " + raizAnonima(16)); //Function Expressions
+
+const raizArrow = n => {
+  return n ** 0.5;
+};
+
+console.log("Arrow Function: " + raizArrow(25));
+
+const arrowSimplificada = n => n ** 0.5;
+
+console.log("Arrow simplificada: " + arrowSimplificada(36));
