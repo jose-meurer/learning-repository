2083db1f7f7e3f5c6999ce905c4e5eed1e4cec05
Jose Meurer <josewmeurer@gmail.com>
2024-05-30 19:57:36 -0300

    js aula21 operadores de comparacao

---
+++ javascript/aula21/index.js
@@ -0,0 +1,22 @@
+/**
+ * Operadores de comparação
+ *
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= menor que ou igual a
+ * == igualdade (valor)
+ * === igualdae estrita (valor e tipo)
+ * != diferente (valor)
+ * !== diferente estrito (valor e tipo)
+ */
+
+console.log(10 > 5);
+
+const num1 = 10;
+const num2 = "10";
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+console.log(num1 != num2);
+console.log(num1 !== num2);
