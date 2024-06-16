20a4759aa669fa4f2d5f16432dd5e85261808d56
Jose Meurer <josewmeurer@gmail.com>
2024-06-16 17:49:27 -0300

    js aula66 map

---
+++ javascript/aula66/index.js
@@ -0,0 +1,33 @@
+// Map => retorna um novo array
+
+// Dobre os números
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+const numerosDobrados = numeros.map(valor => valor * 2);
+console.log(numerosDobrados);
+
+/* 
+Para cada elemento:
+Retorne apenas uma string com o nome da pessoa
+Remova apenas a chave "nome" do objeto
+Adicione uma chave id em cada objeto
+*/
+const pessoas = [
+  { nome: "José", idade: 62 },
+  { nome: "Maria", idade: 23 },
+  { nome: "Eduarco", idade: 55 },
+  { nome: "Letícia", idade: 19 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 47 },
+];
+
+const nomes = pessoas.map(obj => obj.nome);
+
+const idades = pessoas.map(obj => ({ idade: obj.idade })); //Expressao
+
+const comIds = pessoas.map((obj, index) => ({ id: index + 1, ...obj }));
+
+// console.log(nomes);
+// console.log(idades);
+console.log(comIds);
+console.log(pessoas);
