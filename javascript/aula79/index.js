d861171b6c1f693858f03312d7d46f9beff52aee
Jose Meurer <josewmeurer@gmail.com>
2024-06-26 16:57:56 -0300

    js aula79 mixin

---
+++ javascript/aula79/index.js
@@ -0,0 +1,48 @@
+// Factory function - Mixin
+
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  },
+};
+
+const comer = {
+  comer() {
+    console.log(`${this.nome} está comendo.`);
+  },
+};
+
+const beber = {
+  beber() {
+    console.log(`${this.nome} está bebendo.`);
+  },
+};
+
+const pessoaPrototype = Object.assign({}, falar, comer, beber);
+
+function criaPessoa(nome, sobrenome) {
+  return Object.create(pessoaPrototype, {
+    nome: {
+      value: nome,
+      enumerable: true,
+      writable: true,
+    },
+    sobrenome: {
+      value: sobrenome,
+    },
+  });
+}
+
+const p1 = criaPessoa("José", "Meurer");
+const p2 = criaPessoa("Maria", "Brown");
+console.log(Object.getOwnPropertyDescriptors(p2));
+
+function Funcao(nome) {
+  this.nome = "__" + nome;
+
+  return this;
+}
+
+const f1 = Funcao.bind(p2); // retorna uma funcao
+console.log(f1);
+console.log(f1("Luiz"));
