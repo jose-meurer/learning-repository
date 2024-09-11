1ad44372cd5d5cee2911b06194b2588febee1bf1
Jose Meurer <josewmeurer@gmail.com>
2024-09-11 19:51:58 -0300

    java Hora global para local e calculos com horas

---
+++ java/fundamentos/datas/src/CalculosComData.java
@@ -0,0 +1,37 @@
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
+public class CalculosComData {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.parse("2024-10-20");
+        LocalDateTime localDateTime = LocalDateTime.parse("2024-10-20T01:30:00");
+        Instant instant = Instant.parse("2024-10-20T01:30:00Z");
+
+        //Datas sao imutaveis
+        LocalDate semanaPassadaLocalDate = localDate.minusWeeks(1);
+        LocalDate proximaSemanaLocalDate = localDate.plusDays(7);
+
+        System.out.println("LocalDate: ");
+        System.out.println("Semana passada: " + semanaPassadaLocalDate);
+        System.out.println("Proxima semana: " + proximaSemanaLocalDate);
+
+        Instant semanaPassadaInstant = instant.minus(7, ChronoUnit.DAYS);
+        Instant proximaSemanaInstant = instant.plus(7, ChronoUnit.DAYS);
+
+        System.out.println();
+        System.out.println("Instant: ");
+        System.out.println("Semana passada: " + semanaPassadaInstant);
+        System.out.println("Proxima semana: " + proximaSemanaInstant);
+
+        //Duration
+        Duration t1 = Duration.between(semanaPassadaInstant, proximaSemanaInstant);
+        Duration t2 = Duration.between(semanaPassadaLocalDate.atTime(0, 0), proximaSemanaLocalDate.atStartOfDay());
+        Duration t3 = Duration.between(proximaSemanaInstant, semanaPassadaInstant);
+
+        System.out.println();
+        System.out.println("Duration: ");
+        System.out.println("t1 dias = " + t1.toDays());
+        System.out.println("t2 dias = " + t2.toDays());
+        System.out.println("t3 Horas = " + t3.toHours());
+    }
+}
---
+++ java/fundamentos/datas/src/ConverterHoraGlobalParaLocal.java
@@ -0,0 +1,35 @@
+import java.time.*;
+import java.time.temporal.ChronoField;
+
+public class ConverterHoraGlobalParaLocal {
+    public static void main(String[] args) {
+
+        //Lista de fusos que podem ser usados
+//        ZoneId.getAvailableZoneIds().forEach(System.out::println);
+
+        LocalDate localDate = LocalDate.parse("2024-10-20");
+        LocalDateTime localDateTime = LocalDateTime.parse("2024-10-20T01:30:00");
+        Instant instant = Instant.parse("2024-10-20T01:30:00Z");
+
+        //Hora global para hora local
+        LocalDate r1 = LocalDate.ofInstant(instant, ZoneId.systemDefault());
+        LocalDate r2 = LocalDate.ofInstant(instant, ZoneId.of("Europe/Monaco"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(instant, ZoneId.of("Europe/Monaco"));
+
+        System.out.printf("r1: %s%n" +
+                "r2: %s %n" +
+                "r3: %s %n" +
+                "r4: %s %n", r1, r2, r3, r4);
+
+        System.out.println("LocalDate dia: " + localDate.getDayOfMonth());
+        System.out.println("LocalDate Mês(nome): " + localDate.getMonth());
+        System.out.println("LocalDate Mês(número): " + localDate.getMonthValue());
+
+        System.out.println("LocalDateTime Horas: " + localDateTime.getHour());
+        System.out.println("LocalDateTime Minutos: " + localDateTime.getMinute());
+        System.out.println("Instant Hora Atual: " + Instant.now());
+
+
+    }
+}
