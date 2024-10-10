24b8a1be167dcb4b4c2a9acb797670c20e4637da
Jose Meurer <josewmeurer@gmail.com>
2024-10-10 19:04:48 -0300

    java generics delimitados

---
+++ java/fundamentos/generics/Generics/src/delimitados/app/Program.java
@@ -0,0 +1,40 @@
+package delimitados.app;
+
+import delimitados.entities.Product;
+import delimitados.services.CalculationService;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        File path = new File("./in.txt");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            Product x = CalculationService.max(list);
+            System.out.print("Produto com maior preço: " + x);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ java/fundamentos/generics/Generics/src/delimitados/entities/Product.java
@@ -0,0 +1,41 @@
+package delimitados.entities;
+
+public class Product implements Comparable<Product>{
+
+    private String name;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + getName() + " -> " + String.format("%.2f", getPrice()) + " ]";
+    }
+
+    @Override
+    public int compareTo(Product p) {
+        return getPrice().compareTo(p.getPrice());
+    }
+}
