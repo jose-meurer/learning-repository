61dec32d7df8f4de357e98cc8d279b3c43ee2d48
Jose Meurer <josewmeurer@gmail.com>
2024-12-19 11:25:01 -0300

    mongodb first commit

---
+++ java/mongodb/first-project/src/main/java/com/josemeurer/demo/SpringbootWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.josemeurer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithMongodbApplication.class, args);
+	}
+
+}
